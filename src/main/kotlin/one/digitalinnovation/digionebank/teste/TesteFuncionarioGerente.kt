791ec73e7e0fb2eb2bbcf2e6b52850151d4b0cca
Islande <islandefs@gmail.com>
2022-06-14 15:37:59 -0300

    Parte 8 - Polimorfismo

--- src/main/kotlin/one/digitalinnovation/digionebank/teste/TesteFuncionarioGerente.kt
+++ src/main/kotlin/one/digitalinnovation/digionebank/teste/TesteFuncionarioGerente.kt
@@ -2,11 +2,14 @@ package one.digitalinnovation.digionebank.teste
 
 
 import one.digitalinnovation.digionebank.Gerente
+import kotlin.math.log
 
 
 fun main(){
 
-    val maria = Gerente("Maria da Silva", "222.222.222-22",5000.0)
+    val maria = Gerente("Maria da Silva", "222.222.222-22",5000.0, "senha123")
 
     ImprimeRelatorioFuncionario.imprime(maria)
+
+    TesteAutenticacao().autentica(maria)
 }
