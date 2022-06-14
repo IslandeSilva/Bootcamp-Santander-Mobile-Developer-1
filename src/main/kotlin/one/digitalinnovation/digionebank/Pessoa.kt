4acee8299a7ee58304d1b4e5d719a0df4b0ff2aa
Islande <islandefs@gmail.com>
2022-06-14 11:26:18 -0300

    Parte 6 - Abstract Class

--- src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
+++ src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
@@ -1,25 +1,9 @@
 package one.digitalinnovation.digionebank
 
-class Pessoa {
-    var nome: String = "Islande Silva"
+abstract class Pessoa(
+    val nome: String,
+    val cpf: String
+)
 
-    var cpf: String = "000.000.000-00"
-    private set // Mantem privado somente o SET, atribuir valor
 
-   /* inner class endereco {
-        var rua: String = "Rua Teste"
-    }*/ // Inner Class - Classe dentro da Classe
 
-    constructor()
-    fun pessoainfo() = "$nome e $cpf"
-}
-
-fun main(){
-    val islande = Pessoa()
-
-    println(islande.pessoainfo())
-
-   // println(islande.nome)
-   // println(islande.cpf)
-   // println(islande.endereco().rua) //Inner Class - Chamar Classe Interna
-}
